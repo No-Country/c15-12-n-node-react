91b1f759371f49b91bd75925156651540aed789c
erickseis <erickseis@gmail.com>
2023-11-29 11:49:41 -0300

    cambio colores y otros parametros a nivel global

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,36 +1,36 @@
-import "../src/styles/nav.css";
-import portatoIcon from "../src/assets/images_project/Nueva carpeta/guitarra.png";
-import userIcon from "../src/assets/images_project/Nueva carpeta/usuario.png";
-import { Link } from "react-router-dom";
+import '../src/styles/nav.css';
+import portatoIcon from '../src/assets/images_project/Nueva carpeta/guitarra.png';
+import userIcon from '../src/assets/images_project/Nueva carpeta/usuario.png';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <nav>
       <img
-        className="portato-img-icon"
+        className='portato-img-icon'
         src={portatoIcon}
-        alt="portato-img-icon"
+        alt='portato-img-icon'
       />
       <h5>Academia Portato</h5>
       <ul>
         <li>
-          <Link to="/">home</Link>
+          <Link to='/'>Home</Link>
         </li>
         <li>About</li>
         <li>
-          <Link to="/courses">Courses</Link>
+          <Link to='/courses'>Courses</Link>
         </li>
         <li>
-          <Link to="/teacher">Teacher</Link>
+          <Link to='/teacher'>Teacher</Link>
         </li>
         <li>Review</li>
         <li>
-          <Link to="/blog">blog</Link>
+          <Link to='/blog'>blog</Link>
         </li>
         <li>Contact</li>
       </ul>
-      <div className="container-img">
-        <img className="user-img" src={userIcon} alt="user-img" />
+      <div className='container-img'>
+        <img className='user-img' src={userIcon} alt='user-img' />
       </div>
     </nav>
   );
