d67b1b18aaac701f617a8afe8f60155c931dae78
Daisy Castillo <81197151+DaisyCastilloS@users.noreply.github.com>
2023-12-21 20:03:33 -0300

    Update course.models.js

--- servidor/model/course.models.js
+++ servidor/model/course.models.js
@@ -48,6 +48,28 @@ const CourseSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+    email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [5, "El correo electrónico debe tener al menos 5 caracteres"],
+    validate: [
+      {
+        validator: async function (value) {
+          const emailExists = await this.constructor.findOne({ email: value });
+          return !emailExists;
+        },
+        message: "Este correo electrónico ya está registrado",
+      },
+      {
+        validator: function (value) {
+          // Validación de formato de correo electrónico
+          return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
+        },
+        message: "Formato de correo electrónico no válido",
+      },
+    ],
+  },
 });
 
 CourseSchema.pre("validate", function (next) {
