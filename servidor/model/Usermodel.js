e629da7811e0d9ba2fa7730da4055da05fffef05
Daisy Castillo <81197151+DaisyCastilloS@users.noreply.github.com>
2023-12-24 12:24:34 -0300

    relacion rol usuario

--- servidor/model/Usermodel.js
+++ servidor/model/Usermodel.js
@@ -1,11 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-    nombre: String,
-    email: String,
-    descripcion: String,
-});
+const UserSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: [5, "El correo electrónico debe tener al menos 5 caracteres"],
+      validate: [
+        {
+          validator: async function (value) {
+            const emailExists = await this.constructor.findOne({
+              email: value,
+            });
+            return !emailExists;
+          },
+          message: "Este correo electrónico ya está registrado",
+        },
+        {
+          validator: function (value) {
+            // Validación de formato de correo electrónico
+            return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+              value
+            );
+          },
+          message: "Formato de correo electrónico no válido",
+        },
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [6, "La contraseña debe tener al menos 6 caracteres"],
+    },
+    //role se relaciona a user.models,será una colección de roles,mediante la id de rol,uno es a muchos
+    roles: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Role",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false, // This should not be a type, it's just a configuration option
+  }
+);
 
-const UserModel = mongoose.model('Usercontact', userSchema);
+UserSchema.statics.comparePasswords = async (password, receivedPassword) => {
+  return await bcrypt.compare(password, receivedPassword);
+};
 
-module.exports = UserModel;
+module.exports = mongoose.model("User", UserSchema);
